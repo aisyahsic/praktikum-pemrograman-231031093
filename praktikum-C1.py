10ba92e8a9e4cc6e79a604e420bcfecbf04d9419
aisyahsic <153783438+aisyahsic@users.noreply.github.com>
2023-12-14 09:33:09 +0800

    Add files via upload

---
+++ praktikum-C1.py
@@ -0,0 +1,42 @@
+print('praktikum-1')
+print()
+print('Nama lengkap : Aisyah')
+print('NIM          : 231031093')
+print('Prodi        : Sistem Informasi C') 
+
+angkatan=2023
+print('Angkatan adalah',angkatan)
+
+print()
+a = 20
+print('Data adalah =',a)
+print('Tipe adalah = ',type(a))
+
+print()
+b = 20.0
+print('Data adalah =',b)
+print('Tipe data adalah =',type(b))
+
+print()
+kampus = 'Institut Teknologi BJ Habibie'
+print('Data adalah =',kampus)
+print('Tipe data adalah =',type(kampus))
+
+print()
+log = True
+print('Data adalah =',log)
+print('Tipe data adalah =',type(log))
+
+print()
+c = complex(3,4)
+print('Data adalah =',c)
+print('Tipe data adalah =',type(c))
+
+print()
+p = 20
+q = 5
+hasil = p - q
+print('hasil',p,'-',q,'=',hasil)
+
+
+
